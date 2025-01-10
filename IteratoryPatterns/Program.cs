ebcc39e896b11b6b5a0b30d01df87424d5c2dc79
Anand513 <varma.anand513@gmail.com>
2025-01-10 08:23:17 +0530

    updated

--- IteratoryPatterns/Program.cs
+++ IteratoryPatterns/Program.cs
@@ -2,7 +2,7 @@
 BookCollections books=new BookCollections();
 books.AddBook(new Book("DesignPatterns"));
 books.AddBook(new Book("Client Code"));
-
+books.AddBook(new Book("Client Code 1"));
 
 IIterator<Book> iterator = books.CreateIterotor();
 
